ef362392a2b1a303e009f34350e439c3cf4a13e3
Eugene Cherevko <piloteg@list.ru>
2022-09-13 18:02:51 +0600

    initial commit

---
+++ public/js/module/sum.js
@@ -0,0 +1,2 @@
+
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibW9kdWxlL3N1bS5qcyIsInNvdXJjZXMiOltdLCJzb3VyY2VzQ29udGVudCI6W10sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiIifQ==
---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+//const sum = require("./module/sum.js")
+//const owl = require("./libs/owl.carousel.js")
+$(document).ready(function(){
+    $('.slider .owl-carousel').owlCarousel({
+        loop:true,
+        margin: 0,
+        nav:true,
+        dots: false,
+        navText: [],
+        responsive:{
+            0:{
+                items:1,
+                dots: true,
+                nav: false
+            },
+            480:{
+                items:1
+            }
+            
+        }
+    }) 
+})
